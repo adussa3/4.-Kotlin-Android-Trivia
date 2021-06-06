d041209b71926b51c6b5536a528c0ec7a7b3f6fe
Anunoy Dussa <adussa3@gmail.com>
2021-06-05 22:16:10 -0500

    Added navigation for fragments
    1) Added navigation for fragments
    2) Linked the title fragment to the game fragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -16,9 +18,31 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
 class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for the title fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false
         )
+
+        // Complicated method to change fragments
+        // NOTE: These require a view: View parameter
+        /* binding.playButton.setOnClickListener {view: View ->
+            // NOTE: these both do the same thing
+
+            // This works if you import "androidx.navigation.Navigation"
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+
+            // This works if you import "androidx.navigation.findNavController"
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        } */
+
+        // Simpler way to change fragments
+        // This does NOT require a view: View parameter
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+
+        // return the created view
         return binding.root
     }
 }
